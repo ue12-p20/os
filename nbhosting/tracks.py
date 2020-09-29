7f49ed0079d0a85268b1d061cb1893d5cf27f42d
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-09-29 10:57:49 +0200

    add track for nbhosting

---
+++ nbhosting/tracks.py
@@ -0,0 +1,32 @@
+# pylint: disable=c0111
+
+from nbhosting.courses import (
+    Track, Section, Notebook,
+    notebooks_by_pattern, track_by_directory)
+
+def tracks(coursedir):
+    """
+    coursedir is a CourseDir object that points
+    at the root directory of the filesystem tree
+    that holds notebooks
+
+    result is a list of Track instances
+    """
+
+    track_specs = [
+        ('cours #1: os', 'OS & TP on scheduler', '',
+         [
+           ('TP', '1*.md'),
+         ]),
+        ]
+
+    return [Track(coursedir,
+                  [Section(coursedir=coursedir,
+                           name=section_name,
+                           notebooks=notebooks_by_pattern(
+                               coursedir, pattern))
+                   for section_name, pattern in section_specs],
+                  name=track_name,
+                  description=track_description,
+                  id=track_id)
+            for (track_name, track_description, track_id, section_specs) in track_specs]
